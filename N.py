1663067438f24b70700c5ec042ba9ceed712cf06
Ankush8021 <ankushkumar85511@gmail.com>
2025-03-29 13:28:39 +0530

    Update N.py

--- N.py
+++ N.py
@@ -5,7 +5,7 @@ import subprocess
 import datetime
 import os
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('8012442954:AAHNvNPyAC8H551z-0P5TnMfKrKr7qrbi9I')
+bot = telebot.TeleBot('8012442954:AAFqSQi96HDoworssfF6-x0VVJl2TfnGEK8')
 
 # Admin user IDs
 admin_id = {"1929943036"}
